f0cdece90fcdc8c2c0a38dc4a9398369e5b7548a
Stijn Tintel <stijn@linux-ipv6.be>
2018-02-17 22:59:07 +0100

    kernel: bump 4.14 to 4.14.20
    Refresh patches.
    Remove upstreamed patches:
    - backport/080-v4.15-0001-arch-define-weak-abort.patch
    - backport/081-v4.15-0002-kernel-exit.c-export-abort-to-modules.patch
    Update patch that no longer applies:
    pending/493-ubi-set-ROOT_DEV-to-ubiblock-rootfs-if-unset.patch
    
    Fixes CVE-2017-8824.
    
    Compile-tested: octeon, x86/64.
    Runtime-tested: octeon, x86/64.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,12 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .71
 LINUX_VERSION-4.4 = .113
 LINUX_VERSION-4.9 = .82
-LINUX_VERSION-4.14 = .18
+LINUX_VERSION-4.14 = .20
 
 LINUX_KERNEL_HASH-3.18.71 = 5abc9778ad44ce02ed6c8ab52ece8a21c6d20d21f6ed8a19287b4a38a50c1240
 LINUX_KERNEL_HASH-4.4.113 = b772d2898d2a791b5bf8bd597beccef385934e237f6c6bb1cf4c609940af773d
 LINUX_KERNEL_HASH-4.9.82 = 4b710b2701daafeb8e4db4c06f0ed3e62a6d20d6213a4927769c89ae42a9b180
-LINUX_KERNEL_HASH-4.14.18 = 866a94c1c38d923ae18e74b683d7a8a79b674ebdfe7f40f1a3be9a27d39fe354
+LINUX_KERNEL_HASH-4.14.20 = 4ab7f42aa6af9c1e3b00cba6b1fa305a87407666aaa2fae555f7fbdaafb6d292
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
