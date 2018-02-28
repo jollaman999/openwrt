ebe8e979c2d1c7446be403e4d066eb7c9f73a20f
Stijn Segers <foss@volatilesystems.org>
2018-02-28 22:53:37 +0100

    kernel: bump 4.14 to 4.14.23
    This patch bumps the 4.14 kernel to .23.
    - Refreshed patches.
    - Deleted bcm53xx/patches-4.14/089-PCI-iproc-Fix-NULL-pointer-dereference-for-BCMA.patch. Has been accepted upstream.
    - Deleted generic/pending-4.14/821-usb-Remove-annoying-warning-about-bogus-URB.patch. The upstream URB code was changed,
      the patch no longer applies. I discussed this with the patch author and removed it for now, we'll see how it goes.
    
    Compile-tested on: ramips/mt7621, x86/64
    Run-tested on: ramips/mt7621, x86/64
    
    Signed-off-by: Stijn Segers <foss@volatilesystems.org>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -5,12 +5,12 @@ LINUX_RELEASE?=1
 LINUX_VERSION-3.18 = .71
 LINUX_VERSION-4.4 = .113
 LINUX_VERSION-4.9 = .83
-LINUX_VERSION-4.14 = .20
+LINUX_VERSION-4.14 = .23
 
 LINUX_KERNEL_HASH-3.18.71 = 5abc9778ad44ce02ed6c8ab52ece8a21c6d20d21f6ed8a19287b4a38a50c1240
 LINUX_KERNEL_HASH-4.4.113 = b772d2898d2a791b5bf8bd597beccef385934e237f6c6bb1cf4c609940af773d
 LINUX_KERNEL_HASH-4.9.83 = 8957e2879c085ab287f5d99d8226ccc53e5e7ca58a86491bd51facb44e2950b3
-LINUX_KERNEL_HASH-4.14.20 = 4ab7f42aa6af9c1e3b00cba6b1fa305a87407666aaa2fae555f7fbdaafb6d292
+LINUX_KERNEL_HASH-4.14.23 = 9a97b5555a2baaed9aa7711524ae3b9179579efb902d76a814a916aefe5712ca
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
