c7b1e2b4e9b0ca9bdd7d7d47d3c2092158dfec93
jollaman999 <admin@jollaman999.com>
2018-06-06 14:08:52 +0900

    ar71xx: Refresh patches for kernel 4.14.48

--- target/linux/ar71xx/files-4.14/arch/mips/ath79/machtypes.h
+++ target/linux/ar71xx/files-4.14/arch/mips/ath79/machtypes.h
@@ -43,6 +43,7 @@ enum ath79_mach_type {
 	ATH79_MACH_AP90Q,			/* YunCore AP80Q/AP90Q */
 	ATH79_MACH_AP91_5G,			/* ALFA Network AP91-5G */
 	ATH79_MACH_AP96,			/* Atheros AP96 */
+	ATH79_MACH_AP81,			/* Atheros AP81 reference board */
 	ATH79_MACH_ARCHER_C25_V1,		/* TP-LINK Archer C25 V1 board */
 	ATH79_MACH_ARCHER_C5,			/* TP-LINK Archer C5 board */
 	ATH79_MACH_ARCHER_C58_V1,		/* TP-LINK Archer C58 V1 board */
