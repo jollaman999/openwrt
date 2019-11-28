62ad4373ccc2397b42765ec00d6d2e3c8dd8864f
Jiri Kastner <cz172638@gmail.com>
2019-11-28 16:29:31 +0100

    scripts/dowload.pl: add archive.apache.org to apache mirror list
    apache mirrors holds only latest releases, to download
    older releases, one must use archive.apache.org to get
    them.
    
    Signed-off-by: Jiri Kastner <cz172638@gmail.com>

--- scripts/download.pl
+++ scripts/download.pl
@@ -199,6 +199,7 @@ foreach my $mirror (@ARGV) {
 		push @mirrors, "https://mirror.netcologne.de/apache.org/$1";
 		push @mirrors, "https://mirror.aarnet.edu.au/pub/apache/$1";
 		push @mirrors, "https://mirror.csclub.uwaterloo.ca/apache/$1";
+		push @mirrors, "https://archive.apache.org/dist/$1";
 		push @mirrors, "http://mirror.cogentco.com/pub/apache/$1";
 		push @mirrors, "http://ftp.jaist.ac.jp/pub/apache/$1";
 		push @mirrors, "ftp://apache.cs.utah.edu/apache.org/$1";
